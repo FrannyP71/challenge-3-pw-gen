2d247b15732074d7409b51d0e0490ab0fd074f51
FrannyP71 <127154382+FrannyP71@users.noreply.github.com>
2023-04-05 14:16:28 -0400

    added variables needed

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = "0123456789"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcdefghijklmnopqrstuv"
+var special = "!@#$%^&*"
+//
+
+function generatePassword() {
+  var pwLength = window.prompt("Enter length between 8 and 128");
+  console.log(pwLength) 
+
+//check if imput is legal and return if not, window.alert, return ""
+//look up random number generator with a for loop
+
+  var askLower = window.confirm("Do you want to use lowercase?");
+  var askUpper = window.confirm("Do you want to use uppercase?");
+  var askNumeric = window.confirm("Do you want to use numeric?");
+  var askSpecial = window.confirm("Do you want to use special?"); 
+
+  var selectedChars = ""
+
+  var pwText = ""
+//evaluate prompt answers
+
+  if (askNumeric) {
+    selectedChars = selectedChars + numbers
+  }
+
+  return pwText
+}
+
 
 // Write password to the #password input
 function writePassword() {
